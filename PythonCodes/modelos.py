b9575474a52f89a91738fb3c151eaffcae6d0bf0
Gabriel <gabrielvernizi@hotmail.com>
2026-07-08 19:03:25 -0300

    feat: fitting the data is now real, not just a dream

--- PythonCodes/modelos.py
+++ PythonCodes/modelos.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import scipy as scp
+import numpy as np
 
 def modelo_sigmoide(T, A, k, t0, V_base):
-    return V_base + A / (1.0 + scp.e**(k * (T - t0)))
+    return V_base + A / (1.0 + np.exp(k * (T - t0)))
 
 def modelo_potencia(T, S, E):
     return S * T**(E - 1.0)
 
-def modelo_hyper(T, a, alpha,gamma):
-    # Erro
+def modelo_hyper(T, a, alpha, gamma):
     # return (a * T**alpha) / (T**alpha + gamma**alpha) + a 
     return (a * gamma**alpha) / (T**alpha + gamma**alpha)
